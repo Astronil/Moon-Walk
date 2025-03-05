304210c2cec5fc00c1885e2684c8e9ae623cdfbd
Astronil <166193970+Astronil@users.noreply.github.com>
2025-03-05 15:11:30 -0500

    Initial Commit

---
+++ scripts/script.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+
+  const prevButton = document.querySelector('.prev');
+  const nextButton = document.querySelector('.next');
+  
+
+  prevButton.addEventListener('click', function() {
+      plusSlides(-1);
+   });
+  
+  nextButton.addEventListener('click', function() {
+    plusSlides(1);
+  });
+
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  function currentSlide(n) {
+    showSlides((slideIndex = n));
+  }
+
+  function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+  }
+
+  let slideInterval = setInterval(() => {
+    plusSlides(1);
+  }, 20000);
+});
+
